7561ae2680d74e7284bb5996bc63d3a729153635
LaiLaoLanAN <3279664319@qq.com>
2026-04-14 10:17:46 +0800

    Reapply "Merge branch 'main' into pr/1"
    This reverts commit dca5d9ad621af110cf71f0f1de74fabf38cf53c5.

--- Assets/script/MC/MCLeftFoot.cs
+++ Assets/script/MC/MCLeftFoot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MCLeftFoot : MonoBehaviour
 {
     public bool LIsGround;
+    public Collider2D LPlatForm;
     [SerializeField]private float isGroundCheck;
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,7 @@ public class MCLeftFoot : MonoBehaviour
     void Update()
     {
         LIsGround = Physics2D.Raycast(transform.position, Vector2.down, isGroundCheck, LayerMask.GetMask("Ground")) || Physics2D.Raycast(transform.position, Vector2.down, isGroundCheck, LayerMask.GetMask("EmptyGround")) || Physics2D.Raycast(transform.position, Vector2.down, isGroundCheck, LayerMask.GetMask("TimeInteractable"));
+        LPlatForm = Physics2D.Raycast(transform.position, Vector2.down, isGroundCheck, LayerMask.GetMask("TimeInteractable")).collider;
     }
     private void OnDrawGizmos(){
         Gizmos.DrawLine(transform.position,new Vector2(transform.position.x,transform.position.y-isGroundCheck));
